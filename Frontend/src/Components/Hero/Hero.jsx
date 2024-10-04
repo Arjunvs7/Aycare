3af6f1c258b7b5b5afaa20309e693bc135f63c64
Arjun <arjunvs701@gmail.com>
2024-10-04 12:43:38 +0530

    1st review

--- Frontend/src/Components/Hero/Hero.jsx
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
+import hero_image from '../Assets/png1.png'
 const Hero = () => {
   return (
     <div className='hero'>
      <div className="hero-left">
-        <h2>New Arrivals only</h2>
+        <h2> Stay Healthy </h2>
         <div> 
             
             <div className="hero-hand-icon">
-                <p>new</p>
+                <p>Fresh</p>
                 <img src={hand_icon} alt="" />
             </div>
-            <p>collection</p>
+            <p>New </p>
             <p>for everyone</p>
         </div>
         <div className="hero-latest-btn">
-            <div>Latest collection</div>
+            <div>Organic collections</div>
             <img src={arrow_icon} alt="" />
         </div>
      </div>
--- Frontend/src/Pages/LoginSignup.jsx
+++ Frontend/src/Pages/LoginSignup.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup' >
+      <div className="loginsignup-container">
+        <h1>sign up</h1>
+        <div className="loginsignup-fields">
+          <input type='text' placeholder='Yourname' />
+          <input type='email' placeholder='Email Adress' />
+          <input type='password' placeholder='password'/>
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Alredy have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type='checkbox' name='' id='' />
+          <p>By continuing,i agree to use terms & privacy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
